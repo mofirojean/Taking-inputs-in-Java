121deb050492da734609ce2125ade2daadc56431
Mofiro Jean <93985482+Mofirojean@users.noreply.github.com>
2022-03-19 00:46:12 +0100

    Update InputsJava.java

--- InputsJava.java
+++ InputsJava.java
@@ -1,10 +1,13 @@
+// Java program that takes input and displays it
+// first we import the scanner class 
 import java.util.Scanner;
 
 public class InputsJava {
     public static void main(String[] args){
         // working with inputs in Java
         // special object call System.in
-
+        
+        // here we create an object from the scanner class
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter your age in years: ");
